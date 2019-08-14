1a9a9676f6a169f03243a42573dd834ed690b382
Luca Mattivi <luca@smartdomotik.com>
2019-08-14 11:14:39 +0200

    cleanup code

--- logs-nodes.rb
+++ logs-nodes.rb
@@ -1,35 +1,34 @@
 #!/usr/bin/env ruby
-
 require 'json'
 require 'open-uri'
 
 MQTT_USERNAME = ENV['MQTT_USERNAME'] || ''
 MQTT_PASSWORD = ENV['MQTT_PASSWORD'] || ''
-MQTT_SERVER = ENV['MQTT_SERVER'] || ''
+MQTT_SERVER   = ENV['MQTT_SERVER'] || ''
 
-# ----------------- GET NODES ------------------- #
-json = open(MQTT_SERVER + "/api/v2/management/nodes",
-           http_basic_authentication: [MQTT_USERNAME, MQTT_PASSWORD]).read
-nodes_response = JSON.parse(json)
+# get nodes
+json = open(
+  "#{MQTT_SERVER}/api/v2/management/nodes",
+  http_basic_authentication: [MQTT_USERNAME, MQTT_PASSWORD]
+).read
 
-nodes_json = nodes_response['result']
-
-nodes_list = []
-clients_list = []
+nodes_list         = []
+clients_list       = []
 subscriptions_list = []
-nodes_json.map do |node|
+JSON.parse(json)['result'].map do |node|
+  # get clients for this node
+  json = open(
+    "#{MQTT_SERVER}/api/v2/nodes/#{node['name']}/clients?page_size=100000",
+    http_basic_authentication: [MQTT_USERNAME, MQTT_PASSWORD]
+  ).read
+  clients_json = JSON.parse(json)['result']['objects']
 
- # ----------------- GET CLIENTS FOR THIS NODE ------------------- #
- json = open(MQTT_SERVER + "/api/v2/nodes/%{node_name}/clients?page_size=100000" % {node_name: node['name']},
-             http_basic_authentication: [MQTT_USERNAME, MQTT_PASSWORD]).read
- clients_response = JSON.parse(json)
- clients_json = clients_response['result']['objects']
-
- printf("name: \"%s\" uptime: \"%s\" version: \"%s\" node_status: \"%s\" clients_count: \"%s\"\n",
-   node['name'],
-   node['uptime'],
-   node['version'],
-   node['node_status'],
-   clients_json.count)
-
+  printf(
+    "name: \"%s\" uptime: \"%s\" version: \"%s\" node_status: \"%s\" clients_count: \"%s\"\n",
+    node['name'],
+    node['uptime'],
+    node['version'],
+    node['node_status'],
+    clients_json.count
+  )
 end
